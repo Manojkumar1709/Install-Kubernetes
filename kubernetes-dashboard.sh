17bab7606111557e5097f1b3338d2b27dcbdb573
Manojkumar1709 <mm2040922@gmail.com>
2024-11-04 14:09:14 +0530

    Updated Dashboard

--- kubernetes-dashboard.sh
+++ kubernetes-dashboard.sh
@@ -8,3 +8,38 @@ sudo apt-get install helm
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 # Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+
+kubectl expose deployment kubernetes-dashboard-kong --name k8s-dash-svc --type NodePort --port 443 --target-port 8443 -n kubernetes-dashboard
+
+kubectl get svc k8s-dash-svc -n kubernetes-dashboard
+
+#Create a service account and generate a token:  
+#Create a service account and generate a token for the service account dashboard.yaml file
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin-user
+  namespace: kube-system
+  
+
+kubectl apply -f dashboard.yaml
+
+# Create a token for the service account
+kubectl create token admin-user -n kube-system
+
+
+
